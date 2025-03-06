6f7233cae5df5d2d2355d7208d0772a1fb3fb878
Samuel Ofori <sammyofori890@gmail.com>
2025-03-06 19:06:27 +0000

    Create variable.js

---
+++ day-1/variable.js
@@ -0,0 +1,16 @@
+// Declaring variables with different data types
+
+// String
+define name = "Cascade";
+
+// Boolean
+let isCodingFun = true;
+
+// Undefined
+let futureGoal;
+
+// Null
+let emptyValue = null;
+
+// Logging the variables to console
+console.log(name, isCodingFun, futureGoal, emptyValue);
