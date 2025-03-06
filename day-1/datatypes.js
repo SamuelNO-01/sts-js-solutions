0db20552151f9242fca1b5762b2af2eba77f305f
Samuel Ofori <sammyofori890@gmail.com>
2025-03-06 19:08:22 +0000

    Create datatypes.js

---
+++ day-1/datatypes.js
@@ -0,0 +1,11 @@
+// Declaring variables with different data types
+let name = "Cascade"; // String
+let isCodingFun = true; // Boolean
+let futureGoal; // Undefined
+let emptyValue = null; // Null
+
+// Checking data types using typeof operator
+console.log("Type of name:", typeof name);
+console.log("Type of isCodingFun:", typeof isCodingFun);
+console.log("Type of futureGoal:", typeof futureGoal);
+console.log("Type of emptyValue:", typeof emptyValue);
