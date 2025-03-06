f41a476dc8c9d94c38b4f7efcc2a322c85d07a04
Samuel Ofori <sammyofori890@gmail.com>
2025-03-06 19:45:52 +0000

    datatypes.js

--- day-1/datatypes.js
+++ day-1/datatypes.js
@@ -1,3 +1,31 @@
+// Declaring four variables without assigning values
+let var1;
+let var2;
+let var3;
+let var4;
+
+// Declaring variables with assigned values
+let age = 25;
+let country = "Ghana";
+let isStudent = false;
+let height = 1.75;
+
+// Declaring variables to store personal information
+let firstName = "Samuel";
+let lastName = "Ofori";
+let maritalStatus = "Single";
+let nationality = "Ghanaian";
+let userAge = 22;
+
+// Declaring variables to store personal information in a single line
+let firstName = "Dangote", lastName = "User", maritalStatus = "Single", nationality = "Ghanaian", userAge = 25;
+
+// Declaring two variables for age comparison
+let myAge = 25;
+let yourAge = 30;
+console.log("I am " + myAge + " years old.");
+console.log("You are " + yourAge + " years old.");
+
 // Declaring variables with different data types
 let name = "Cascade"; // String
 let isCodingFun = true; // Boolean
